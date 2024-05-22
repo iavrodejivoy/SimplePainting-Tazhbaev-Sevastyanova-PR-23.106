507bed515072d372977c768651b59e83ce53cf8f
iavrodejivoy <timurovv838@gmail.com>
2024-05-22 10:44:39 +0700

    Commit

---
+++ app/src/main/java/com/example/simplepainting/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.simplepainting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Draw2D draw2D = new Draw2D(this);
+        setContentView(draw2D);
+    }
+}
